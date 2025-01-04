a2d6ba94e288cf4340d033be6cf036fa3902ccd7
meemansha07 <meemanshasingh77@gmail.com>
2025-01-04 23:33:53 +0530

    Added authentication and authorization

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { postUser } from "../controllers/userController";
-import { getUsers } from "../controllers/userController";
+import { getUser, getUsers, postUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get('/', getUsers);
 router.post("/", postUser);
+router.get("/:cognitoId", getUser);
 
 export default router;
